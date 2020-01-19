809ec93640324b186f8d6d3c0b394f592702544d
Tsubasa Otsu <26428614+kamome283@users.noreply.github.com>
2020-01-19 21:37:54 +0900

    Add copy macro

--- src/Dataclass.hx
+++ src/Dataclass.hx
@@ -2,77 +2,110 @@ import haxe.macro.Expr;
 import haxe.macro.Context;
 
 class Dataclass {
-    static public macro function constructor(): Array<Field> {
-        var fields = Context.getBuildFields();
-        var args: Array<FunctionArg> = [];
-        var exprs: Array<Expr> = [];
+	static public macro function constructor():Array<Field> {
+		var fields = Context.getBuildFields();
+		var args:Array<FunctionArg> = [];
+		var exprs:Array<Expr> = [];
 
-        for (field in fields) {
-            switch (field.kind) {
-                case FVar(t, e):
-                    final name = field.name;
-                    final arg = {name: name, type: t};
-                    args.push(arg);
-                    final expr = macro this.$name = $i{name};
-                    exprs.push(expr);
-                default:
-            }
-        }
+		for (field in fields) {
+			switch (field.kind) {
+				case FVar(t, e):
+					final name = field.name;
+					final arg = {name: name, type: t};
+					args.push(arg);
+					final expr = macro this.$name = $i{name};
+					exprs.push(expr);
+				default:
+			}
+		}
 
-        final func: Function = {
-            args: args,
-            ret: (macro:Void),
-            expr: macro $b{exprs},
-        };
+		final func:Function = {
+			args: args,
+			ret: (macro:Void),
+			expr: macro $b{exprs},
+		};
 
-        final field: Field = {
-            name: "new",
-            kind: FFun(func),
-            access: [Access.APublic],
-            pos: Context.currentPos(),
-        };
+		final field:Field = {
+			name: "new",
+			kind: FFun(func),
+			access: [Access.APublic],
+			pos: Context.currentPos(),
+		};
 
-        fields.push(field);
-        return fields;
-    }
+		fields.push(field);
+		return fields;
+	}
 
-    static public macro function makePublic(): Array<Field> {
-        var fields = Context.getBuildFields();      
-        for (field in fields) {
-            switch (field.kind) {
-                case FVar(_):
-                    field.access.remove(Access.APrivate);
-                    field.access.push(Access.APublic);
-                default:
-            }
-        }
-        return fields;
-    }
+	static public macro function makePublic():Array<Field> {
+		var fields = Context.getBuildFields();
+		for (field in fields) {
+			switch (field.kind) {
+				case FVar(_):
+					field.access.remove(Access.APrivate);
+					field.access.push(Access.APublic);
+				default:
+			}
+		}
+		return fields;
+	}
 
-    static public macro function copy(): Array<Field> {
-        final classType = Context.getClassPath();
-        var fields = Context.getBuildFields();
-        var args: Array<FunctionArg> = [];
-        var types: Array<ComplexType> = [];
-        var exprs: Array<Expr> = [];
-        for (field in fields) {
-            switch (field.kind) {
-                case FVar(t, e):
-                    final name = field.name;
-                    final type: ComplexType = TNamed(name, t);
-                    types.push(type);
-                    final expr = macro fields.$name == null ? this.$name : fields.$name;
-                    exprs.push(expr);
-                default:
-            }
-        }
-        final type = TOptional(TIntersection(types));
-        final arg: FunctionArg = {name: "fields", type: type};
-        final func: Function = {
-            args: [arg],
-            ret: null, // return Self
-            expr: macro return new $classType(${exprs[0]}, ${exprs[1]})
-        };
-        return fields;
-    }
+	static public macro function copy():Array<Field> {
+		var classFields = Context.getBuildFields();
+
+		var types:Array<ComplexType> = [];
+		var params:Array<Expr> = [];
+		for (f in classFields) {
+			switch (f.kind) {
+				case FVar(t, e):
+					final name = f.name;
+					var t = ComplexType.TAnonymous([
+						{
+							name: name,
+							pos: Context.currentPos(),
+							kind: FVar(t),
+							meta: [
+								{
+									name: ':optional',
+									params: [],
+									pos: f.pos
+								}
+							]
+						}
+					]);
+					types.push(t);
+					params.push(macro fields.$name == null ? this.$name : fields.$name);
+				default:
+			}
+		}
+
+		final typepath:TypePath = {
+			var pack = [];
+			final name = Context.getLocalClass().get().name;
+			{name: name, pack: pack}
+		};
+
+		final funcExpr:Expr = {
+			final constructExpr:Expr = {expr: ENew(typepath, params), pos: Context.currentPos()};
+
+			macro {
+				return $constructExpr;
+			}
+		}
+
+		final func:Function = {
+			args: [{name: "fields", type: TIntersection(types)}],
+			ret: TPath(typepath),
+			expr: funcExpr,
+		}
+
+		final field:Field = {
+			name: "copy",
+			kind: FFun(func),
+			access: [Access.APublic],
+			pos: Context.currentPos(),
+		};
+
+		classFields.push(field);
+		return classFields;
+	}
 }
--- src/Main.hx
+++ src/Main.hx
@@ -1,31 +1,21 @@
-import Dataclass;
+using Dataclass;
 
 class Main {
     static public function main() {
         final p1 = new Person("Tsubasa", 24);
-        trace(p1.title);
-        trace(p1.name);
-        trace("Hello, World!");
+        final p2 = p1.copy({name: "Kana"});
+        trace(p2.title);
     }
 }
 
+
 @:build(Dataclass.constructor())
 @:build(Dataclass.makePublic())
+@:build(Dataclass.copy())
 class Person {
     final name: String;
     final age: Int;
     public var title(get, never): String;
     inline private function get_title()
         return '${name}(${age})';
-
-    public function copy(?fields: {?name: String, ?age: Int}): Person {
-        if (fields == null) {
-            return copy({});
-        }
-
-        return new Person(
-            fields.name == null ? name : fields.name,
-            fields.age == null ? age : fields.age
-        );
-    }
 }
